6cae770cdb9937ef9a14618bb39c4e53e94fe4f9
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2022-11-28 18:49:25 +0100

    work in progress

--- js/assets.js
+++ js/assets.js
@@ -1,10 +1,26 @@
 
-// Millenium Falcon
+// Player: Millenium Falcon
 const falcon = new Image();
 falcon.src = './img/falcon.webp';
 
-// Bonus
+
+// - Bonus: Yoda & Cheba
+
+// * Yoda
+const yoda = new Image();
+yoda.src = '/img/yoda.jpg';
+
+// * Cheba
+const cheba = new Image();
+cheba.src = '/img/chewa.jpg';
 
 
-// Enemies
+// - Enemies: Bad & Aste
 
+// * Bad
+const bad = new Image();
+bad.src = '/img/bad.webp';
+
+// * Aste
+const aste = new Image();
+aste.src = '/img/aste.webp';
